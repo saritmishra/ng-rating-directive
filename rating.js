896d859d5926f0c20ac78e06b5bf18c0d01d65ac
Sarit Mishra <sarit.mishra@gmail.com>
2014-05-18 16:30:07 -0700

    Cleaning up
    Final version available at http://jsfiddle.net/saritmishra/V4j6z/

--- rating.js
+++ rating.js
@@ -6,23 +6,18 @@ angular.module('directiveTest', []);
 angular.module('directiveTest').directive('rating', function(){
     // Runs during compile
     return {
-        // name: '',
-        // priority: 1,
-        // terminal: true,
+
         scope: {
             ratingValue : '='
         }, // {} = isolate, true = child, false/undefined = no change
-        // controller: function($scope, $element, $attrs, $transclude) {},
-        // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+
         restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
+
         template:
             '<ul>' +
-                '<li ng-repeat="star in stars" class="{{ star.filled }}">\u2605</li>' +
+                '<li ng-repeat="star in stars" ng-class="star.status" ng-click="setRating($index)">\u2605</li>' +
             '</ul>',
-        // templateUrl: '',
-        // replace: true,
-        // transclude: true,
-        // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+
         link: function($scope, iElm, iAttrs, controller) {
 
             $scope.maxStars = 5;
@@ -31,22 +26,22 @@ angular.module('directiveTest').directive('rating', function(){
                 $scope.stars = [];
                 for (var i = 0; i < $scope.maxStars; i += 1) {
                     $scope.stars.push({
-                        filled: (i < $scope.ratingValue) ? 'filled' : ''
+                        status: (i < $scope.ratingValue) ? 'filled' : ''
                     });
                 }
             };
-
             $scope.updateRating();
+
             $scope.$watch('ratingValue', function(newValue, oldValue, scope) {
-                console.log('watch triggered');
                 if (newValue) {
-                    console.log('watch and updaterating');
                     $scope.updateRating();
                 }
             });
 
-
-
+            $scope.setRating = function(index){
+                $scope.ratingValue = index + 1;
+                // $scope.updateRating();
+            };
         }
     };
 });
